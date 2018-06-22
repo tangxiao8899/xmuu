72122244f367f46db0e87f280b9d97435a1572da
zhl123 <598871671@qq.com>
2018-06-23 00:01:17 +0800

    add config

--- src/main/java/com/carryit/base/besttmwuu/BesttmwuuApplication.java
+++ src/main/java/com/carryit/base/besttmwuu/BesttmwuuApplication.java
@@ -3,10 +3,17 @@ package com.carryit.base.besttmwuu;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan("com.carryit.base.besttmwuu.dao")
-public class BesttmwuuApplication {
+public class BesttmwuuApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(BesttmwuuApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(BesttmwuuApplication.class, args);
