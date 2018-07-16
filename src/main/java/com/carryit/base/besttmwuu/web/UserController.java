4fcc8f22ba97c7576e5e15037fa14e7008a15138
caobaoxian <2955193647@qq.com>
2018-07-16 18:51:13 +0800

    添加了增加用户的方法cbx

--- src/main/java/com/carryit/base/besttmwuu/web/UserController.java
+++ src/main/java/com/carryit/base/besttmwuu/web/UserController.java
@@ -4,9 +4,12 @@ import com.carryit.base.besttmwuu.entity.User;
 import com.carryit.base.besttmwuu.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/user")
@@ -20,5 +23,17 @@ public class UserController {
         int userId = Integer.parseInt(request.getParameter("id"));
         User user = this.userService.getUserById(userId);
         return user;
+    }
+
+    @RequestMapping(value = "/addUser",method = RequestMethod.POST)
+    public Map<String, Object> addUser(User user) {
+        Map<String,Object> param = new HashMap<String,Object>();
+        boolean flg = userService.addUser(user);
+        if (flg){
+            param.put("success", "成功");
+        }else {
+            param.put("error", "失败");
+        }
+        return param;
     }
 }
