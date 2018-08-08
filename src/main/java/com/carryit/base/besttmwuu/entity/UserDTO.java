d27c229583c94cdc6e0fc9246d14fb0a2beb8904
chenzhisheng555 <17762369527@163.com>
2018-08-08 22:54:01 +0800

    提交每日上榜

--- src/main/java/com/carryit/base/besttmwuu/entity/UserDTO.java
+++ src/main/java/com/carryit/base/besttmwuu/entity/UserDTO.java
@@ -7,6 +7,24 @@ public class UserDTO implements Serializable {
     private String realName;  //名字
     private String profilePhoto; //头像
     private String WealthValue; //财富值
+    private String sincerity; //诚信值
+    private Integer seq;
+
+    public Integer getSeq() {
+        return seq;
+    }
+
+    public void setSeq(Integer seq) {
+        this.seq = seq;
+    }
+
+    public String getSincerity() {
+        return sincerity;
+    }
+
+    public void setSincerity(String sincerity) {
+        this.sincerity = sincerity;
+    }
 
     public String getRealName() {
         return realName;
---
+++ src/main/java/com/carryit/base/besttmwuu/service/impl/WealthServiceImpl.java
@@ -0,0 +1,9 @@
+package com.carryit.base.besttmwuu.service.impl;
+
+import com.carryit.base.besttmwuu.service.WealthService;
+import org.springframework.stereotype.Service;
+
+@Service("wealthService")
+public class WealthServiceImpl implements WealthService {
+
+}
