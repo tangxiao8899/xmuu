3b8cdbc370d301ae7b6f9550f27139c08c9cbe51
chenzhisheng555 <17762369527@163.com>
2018-10-13 14:48:50 +0800

    修改多文件上传

--- src/main/java/com/carryit/base/besttmwuu/web/ImageController.java
+++ src/main/java/com/carryit/base/besttmwuu/web/ImageController.java
@@ -3,107 +3,75 @@ package com.carryit.base.besttmwuu.web;
 
 import com.alibaba.fastjson.JSONObject;
 import com.base.BaseController;
+import com.base.ResultPojo;
 import com.bean.req.BoardReq;
 import com.carryit.base.besttmwuu.entity.*;
 import com.carryit.base.besttmwuu.service.GlobounService;
 import com.carryit.base.besttmwuu.service.MemberService;
 import com.carryit.base.besttmwuu.service.SincerityService;
+import com.mysql.jdbc.StringUtils;
 import com.util.UploadHelper;
 import org.apache.commons.codec.binary.Base64;
+import org.apache.tomcat.util.http.fileupload.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 import sun.misc.BASE64Decoder;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 @RestController
 @RequestMapping("/image")
-public class ImageController {
+public class ImageController extends BaseController {
 
     @Value(value = "${domain_name}")
     private String domain_name;
 //
 //    //上传图片,先临时保存在/static/index_img下,做测试.后面保存到图片服务器
-//    @RequestMapping(value = "/upload", method = {RequestMethod.GET,
-//            RequestMethod.POST}, produces = "application/json;charset=UTF-8")
-//    public JSONObject save(@RequestBody(required = false) String json) {
-//        return callHttpReqTask(json, 0);
-//    }
-//
-
-
-//    @Override
-//    public JSONObject runTask(String json, int cmd) {
-//        switch (cmd) {
-//
-//            case 0:
-//                Image image = p(json, Image.class);
-//                if(image==null){
-//                    return faild("参数为空~", false);
-//                }
-//                if (image.getBase64() == null) { //图像数据为空
-//                    return faild("参数为空~", false);
-//                }
-//                String path = "/var/apache-tomcat-8.5.31/webapps/besttmwuu-0.0.1/WEB-INF/classes/static/index_img/";
-//                byte[] base64Bytes = Base64.decodeBase64(image.getBase64());
-//                //生成文件名,仅供测试,不保证唯一
-//                String files = new SimpleDateFormat("yyyyMMddHHmmssSSS").format(new Date())
-//                        + (new Random().nextInt(9000) % (9000 - 1000 + 1) + 1000) + ".jpg";
-//                 path = path+files;
-//
-//                OutputStream out = null;
-//                try {
-//                    out = new FileOutputStream(path);
-//                    out.write(base64Bytes);
-//                    out.flush();
-//                    out.close();
-//                    String url = domain_name + "/index_img/" + files;
-//                    return doObjResp(url);
-//                } catch (Exception e) {
-//                    e.printStackTrace();
-//                    return faild("异常~", false);
-//                }
-//
-//                }
-//        return null;
-//
-//        }
 
     @RequestMapping(value="/upload", method = RequestMethod.POST)
     @ResponseBody
-    public String uploadFile(HttpServletRequest request,
+    public JSONObject uploadFile(HttpServletRequest request,
                              HttpServletResponse response) {
-        List<MultipartFile> multipartFiles = UploadHelper.getFileSet(request, 1024 * 20, null);
-        //String path = "/var/apache-tomcat-8.5.31/webapps/besttmwuu-0.0.1/WEB-INF/classes/static/index_img/";
-       String path ="D:\\";
-       if(multipartFiles.size()==0){
-           return null;
-       }
-        for (MultipartFile multipartFile : multipartFiles) {
-            try {
-                String filePath = UploadHelper.uploadFile(multipartFile, path);
-                System.out.println(filePath);
-            } catch (Exception e) {
-                e.printStackTrace();
+        try {
+            MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
+            Map<String, MultipartFile> fileMap = multipartRequest.getFileMap();
+            if(fileMap == null || fileMap.size() == 0){
+                return faild("文件为空",false);
             }
-            // 拿到的imgPath就是图片的相对于contextPath的存储路径了
+            Collection<MultipartFile> files = fileMap.values();
+            for(MultipartFile file:files){
+                File tempFile = getTmpFile();
+                        tempFile.createNewFile();
+                        file.transferTo(tempFile); //到这里tempFile即是上传上来的文件。
+                    }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return faild("上传失败",false);
+
         }
         return null;
     }
 
+    public File getTmpFile() {
+       String path ="/var/apache-tomcat-8.5.31/webapps/besttmwuu-0.0.1/WEB-INF/classes/static/index_img/";
 
+        String filesName = new SimpleDateFormat("yyyyMMddHHmmssSSS").format(new Date())
+                        + (new Random().nextInt(9000) % (9000 - 1000 + 1) + 1000) + ".jpg";
+        return new File(path, filesName);
     }
+
+    @Override
+    public JSONObject runTask(String json, int cmd) {
+        return null;
+    }
+}
 
