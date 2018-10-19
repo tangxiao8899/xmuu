a237aeae9ba8d72fe25daf1862cc30431feeaad5
tangxiao8899 <tangxiao8899@126.com>
2018-10-19 17:31:53 +0800

    优化自动登录

--- src/main/java/com/carryit/base/besttmwuu/service/impl/LoginServiceImpl.java
+++ src/main/java/com/carryit/base/besttmwuu/service/impl/LoginServiceImpl.java
@@ -39,12 +39,20 @@ public class LoginServiceImpl implements LoginService {
 //            HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
 //            String cookie=request.getHeader("cookie");
             Cookie[] cookies = request.getCookies();
+            if(cookies == null || cookies.length == 0){
+                jo.put("code",401);
+                jo.put("msg","首次登陆，请输入用户密码！");
+                jo.put("data","");
+                return jo;
+            }
+
             for(Cookie c : cookies){
                 if(c.getName().equals("token")){ //存在token，校验token正确性
                     String key = c.getValue();
                     if(token.equals(key)){
                         JSONObject data = new JSONObject();
                         data.put("token",key);
+                        data.put("uid",key.split("_")[0]);
                         jo.put("code",200);
                         jo.put("msg","登录成功");
                         jo.put("data",data);
@@ -53,14 +61,14 @@ public class LoginServiceImpl implements LoginService {
                         //token错误，重新登录
                         jo.put("code",401);
                         jo.put("msg","token错误，请重新登录");
-                        jo.put("data",null);
+                        jo.put("data","");
                         return jo;
                     }
                 }else{ //不存在token 说明token过期
                     //重新登录
                     jo.put("code",401);
                     jo.put("msg","token过期，请重新登录");
-                    jo.put("data",null);
+                    jo.put("data","");
                     return jo;
                 }
             }
@@ -68,24 +76,24 @@ public class LoginServiceImpl implements LoginService {
             if(StringUtils.isEmpty(phone) || StringUtils.isEmpty(password)){
                 jo.put("code",400);
                 jo.put("msg","请求参数异常");
-                jo.put("data",null);
+                jo.put("data","");
                 return jo;
             }
             User user = userService.selectByPhone(phone);
             if(user == null){ //用户不存在
                 jo.put("code",401);
                 jo.put("msg","用户不存在");
-                jo.put("data",null);
+                jo.put("data","");
                 return jo;
             }else if(!password.equals(user.getPassword())){ //密码错误
                 jo.put("code",401);
                 jo.put("msg","密码错误");
-                jo.put("data",null);
+                jo.put("data","");
                 return jo;
             }else{ //登录成功
                 JSONObject data = new JSONObject();
 
-                String k = user.getUid() + user.getPhone() + System.currentTimeMillis();
+                String k = user.getUid()+"_"+ user.getPhone() + System.currentTimeMillis();
                 Cookie cookie = new Cookie("token",k);
                 cookie.setMaxAge(10*24*60*60); //设置token有效期为10天
                 cookie.setPath("/");
