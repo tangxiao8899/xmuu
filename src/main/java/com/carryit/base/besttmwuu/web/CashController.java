fd2dcec36ca64ede4ce9f45f60475b74b15d67f9
tangxiao8899 <tangxiao8899@126.com>
2018-11-11 10:45:47 +0800

    修改后台登录功能

--- src/main/java/com/carryit/base/besttmwuu/web/CashController.java
+++ src/main/java/com/carryit/base/besttmwuu/web/CashController.java
@@ -1,16 +1,15 @@
 package com.carryit.base.besttmwuu.web;
 
+import com.alibaba.fastjson.JSONObject;
 import com.carryit.base.besttmwuu.entity.User;
 import com.carryit.base.besttmwuu.service.UserService;
-import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -32,18 +31,31 @@ public class CashController extends HttpServlet {
 
     @RequestMapping(value = "/txLogin")
     @ResponseBody
-    public String txLogin(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    public JSONObject txLogin(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        JSONObject jo = new JSONObject();
         String phone=req.getParameter("phone");
         String password=req.getParameter("password");
-        if(StringUtils.isBlank(phone)||StringUtils.isBlank(password)){
-            return "账号或者密码不能为空";
+        if(StringUtils.isEmpty(phone)||StringUtils.isEmpty(password)){
+            jo.put("code",400);
+            jo.put("msg","账号或者密码不能为空");
         }
-        User user=new User();
-        user=userService.getUserByPoneAndPassword(phone);
-        if(password.equals(user.getPassword())){
-            return "密码错误";
+        User user = userService.getUserByPoneAndPassword(phone);
+        if(StringUtils.isEmpty(user)){
+            jo.put("code",404);
+            jo.put("msg","用户不存在");
+        }else if(!password.equals(user.getPassword())){
+            jo.put("code",401);
+            jo.put("msg","密码不正确");
         }else {
-            return "登录成功";
+            jo.put("code",200);
+            jo.put("msg","登录成功");
         }
+        return jo;
+    }
+
+    @RequestMapping(value = "/index")
+    public String index(){
+        logger.info("=====");
+        return "index";
     }
 }
--- src/main/resources/static/assets/login.js
+++ src/main/resources/static/assets/login.js
@@ -1,16 +1,41 @@
 layui.define(['element'],function(exports){
 
-    var $ = layui.$;
-    $('.input-field').on('change',function(){
-        var $this = $(this),
-            value = $.trim($this.val()),
-            $parent = $this.parent();
+//    var $ = layui.$;
+//    $('.input-field').on('change',function(){
+//        var $this = $(this),
+//            value = $.trim($this.val()),
+//            $parent = $this.parent();
+//
+//        if(value !== '' && !$parent.hasClass('field-focus')){
+//            $parent.addClass('field-focus');
+//        }else{
+//            $parent.removeClass('field-focus');
+//        }
+//    })
+//    exports('login');
 
-        if(value !== '' && !$parent.hasClass('field-focus')){
-            $parent.addClass('field-focus');
-        }else{
-            $parent.removeClass('field-focus');
-        }
-    })
-    exports('login');
+
 });
+
+        function login() {
+        var $ = layui.$;
+            $.ajax({
+                type: "POST",
+                dataType: "json",
+                url: "txLogin" ,
+                data: {
+                    "phone":$("#phone").val(),
+                    "password":$("#password").val()
+                },
+                success: function (result) {
+                    if (result.code == 200) {
+                        location.href="index"
+                    }else{
+                        alert(result.msg);
+                        }
+                },
+                error : function() {
+                    alert("异常！");
+                }
+            });
+        }
